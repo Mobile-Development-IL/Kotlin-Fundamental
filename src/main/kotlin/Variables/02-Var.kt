34d3935a501c64948031003ecd8a0c91fb9fc3aa
rehanzhakyy <hannn9491@gmail.com>
2024-05-29 15:14:52 +0700

    add Var Implementation

---
+++ src/main/kotlin/Variables/02-Var.kt
@@ -0,0 +1,10 @@
+package org.example.Variables
+
+fun main() {
+    // Var Implementation
+    var company: String = "Infinite Learning"
+    company = "Nongsa Digital"
+
+    // Print Var Implementation
+    println(company)
+}
