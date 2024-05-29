d92a8556029d2b4e4e6690b3ef9d33898d9e5418
rehanzhakyy <hannn9491@gmail.com>
2024-05-29 15:21:19 +0700

    Initialize Data Types

---
+++ src/main/kotlin/DataTypes/01-Introduction.kt
@@ -0,0 +1,9 @@
+package org.example.DataTypes
+
+fun main() {
+    // Initialize Data Types
+    val firstWord = "Infinite"
+    val secondWord = "Learning"
+    // Print Data Types
+    println(firstWord + " " + secondWord)
+}
