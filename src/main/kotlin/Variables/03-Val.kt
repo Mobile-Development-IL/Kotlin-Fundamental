dcb73f1bdeaf18daa6fb42204515b18eeebb9876
rehanzhakyy <hannn9491@gmail.com>
2024-05-29 15:17:56 +0700

    add Val Implementation

---
+++ src/main/kotlin/Variables/03-Val.kt
@@ -0,0 +1,12 @@
+package org.example.Variables
+
+fun main() {
+    // Val Implementation
+    val company: String = "Infinite Learning"
+
+    // Val cannot be reassigned
+    // Uncomment this code to see the Error
+    // company = "Nongsa Digital"
+
+    println(company)
+}
