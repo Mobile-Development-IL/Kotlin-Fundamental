fd37a61d7ae078b484fce866c063d5e2d3fac8c1
rehanzhakyy <hannn9491@gmail.com>
2024-06-11 18:24:05 +0700

    add character

---
+++ src/main/kotlin/DataTypes/02-Character.kt
@@ -0,0 +1,17 @@
+package org.example.DataTypes
+
+fun main() {
+    // Char
+    var grade: Char = 'A'
+    println(grade)
+
+    // Increment
+    println("Grade : " + grade++)
+    println("Grade : " + grade++)
+    println("Grade : " + grade++)
+    println("")
+    // Decrement
+    println("Grade : " + grade--)
+    println("Grade : " + grade--)
+    println("Grade : " + grade--)
+}
