7aea3c50476e43487c05284a3a66d776ffe7b079
rehanzhakyy <hannn9491@gmail.com>
2024-06-11 18:42:51 +0700

    add Boolean Expression

---
+++ src/main/kotlin/DataTypes/05-BooleanExpression.kt
@@ -0,0 +1,21 @@
+package org.example.DataTypes
+
+fun main() {
+    // Boolean Expression
+    val x: Int = 50
+    val y: Int = 24
+
+    // More than
+    println("x > y = " + (x > y))
+    // Less than
+    println("x < y = " + (x < y))
+    // Greater than or equal to
+    println("x >= y = " + (x >= y))
+    // Less than or equal to
+    println("x <= y = " + (x <= y))
+    // Equal
+    println("x == y = " + (x == y))
+    // Not equal
+    println("x != y = " + (x != y))
+
+}
