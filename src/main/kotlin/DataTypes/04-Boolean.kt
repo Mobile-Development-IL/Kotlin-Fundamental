32c1c15a425bc780f953d7992a437e62e39510a8
rehanzhakyy <hannn9491@gmail.com>
2024-06-11 18:35:36 +0700

    add Boolean

---
+++ src/main/kotlin/DataTypes/04-Boolean.kt
@@ -0,0 +1,12 @@
+package org.example.DataTypes
+
+fun main() {
+    // Boolean
+    val x = true
+    val y = false
+
+    println("x && y = ${x && y}")
+    println("x || y = ${x || y}")
+    println("!y = ${!y}")
+
+}
